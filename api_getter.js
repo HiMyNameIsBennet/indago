0158b8f5900dea22f55cdcf66f618f846c983fa0
SushiJackal <weingaben@gmail.com>
2021-09-24 05:36:57 +0200

    first commit

---
+++ api_getter.js
@@ -0,0 +1,21 @@
+const Web3 = require('web3')
+const web3 = new Web3('http://127.0.0.1:8545')
+
+const GetBlockData = async () => {
+    let tx_array = []
+    const block_data = await web3.eth.getBlock("latest")
+    for(tx of block_data.transactions){
+        const tx_data = await web3.eth.getTransaction(tx)
+        if(tx_data?.value !== '0' && !isNaN(tx_data?.value)) {
+            tx_array.push(parseInt(tx_data?.value?.substr(0,7)))
+        }
+    }
+    var max = Math.max.apply(Math, tx_array)
+    var min = Math.min.apply(Math, tx_array)
+    tx_array = tx_array.map(tx => (tx - min) * (0.8 + 0.2) / (max - min) - 0.2)
+    return tx_array
+}
+
+GetBlockData().then(data => {
+    console.log(data)
+})
